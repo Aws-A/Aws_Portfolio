586bcdfb7a549fb5724b10d5b8da3006c3fd7a63
Aws-star <awsalrahim@yahoo.com>
2023-07-07 02:01:53 -0400

    Home Page

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function changePhoto1() {
+  let mobile = document.getElementById('mockup');
+  mobile.style.backgroundImage = "url('./images/enviroHP.png')";
+}
+
+function changePhoto2() {
+  let mobile = document.getElementById('mockup');
+  mobile.style.backgroundImage = "url('./images/gardengateHP.png')";
+}
+
+function changePhoto3() {
+  let mobile = document.getElementById('mockup');
+  mobile.style.backgroundImage = "url('./images/travelzHP.png')";
+}
