874ddc05cbcd7621b92f194a9b4e27cb55654723
Aws-A <awsalrahim@yahoo.com>
2024-12-09 15:47:24 -0500

    Adding Buttons

--- js/index.js
+++ js/index.js
@@ -43,20 +43,20 @@ function switchTo1() {
   let wireframeEN3 = document.getElementById('enviroW3');
   let wireframeEN4 = document.getElementById('enviroW4');
   let wireframeEN5 = document.getElementById('enviroW5');
-  if (document.getElementById('enviroTo').textContent === 'Wireframes (Click to switch to prototypes)'){
+  if (document.getElementById('enviroTo').textContent === 'Show Prototypes'){
     wireframeEN1.style.backgroundImage = "url('./images/enviroPr1.png')";
     wireframeEN2.style.backgroundImage = "url('./images/enviroPr2.png')";
     wireframeEN3.style.backgroundImage = "url('./images/enviroPr3.png')";
     wireframeEN4.style.backgroundImage = "url('./images/enviroPr4.png')";
     wireframeEN5.style.backgroundImage = "url('./images/enviroPr5.png')";
-    document.getElementById('enviroTo').textContent = "Prototypes (Click to switch to wireframes)";
+    document.getElementById('enviroTo').textContent = "Show Wireframes";
   } else {
     wireframeEN1.style.backgroundImage = "url('./images/enviroW1.png')";
     wireframeEN2.style.backgroundImage = "url('./images/enviroW2.png')";
     wireframeEN3.style.backgroundImage = "url('./images/enviroW3.png')";
     wireframeEN4.style.backgroundImage = "url('./images/enviroW4.png')";
     wireframeEN5.style.backgroundImage = "url('./images/enviroW5.png')";
-    document.getElementById('enviroTo').textContent = "Wireframes (Click to switch to prototypes)";
+    document.getElementById('enviroTo').textContent = "Show Prototypes";
   }
 
 
@@ -68,20 +68,20 @@ function switchTo2() {
   let wireframeGG3 = document.getElementById('gardengateW3');
   let wireframeGG4 = document.getElementById('gardengateW4');
   let wireframeGG5 = document.getElementById('gardengateW5');
-  if (document.getElementById('gardengateTo').textContent === 'Wireframes (Click to switch to prototypes)') {
+  if (document.getElementById('gardengateTo').textContent === 'Show Prototypes') {
     wireframeGG1.style.backgroundImage = "url('./images/gardengatePr1.png')";
     wireframeGG2.style.backgroundImage = "url('./images/gardengatePr2.png')";
     wireframeGG3.style.backgroundImage = "url('./images/gardengatePr3.png')";
     wireframeGG4.style.backgroundImage = "url('./images/gardengatePr4.png')";
     wireframeGG5.style.backgroundImage = "url('./images/gardengatePr5.png')";
-    document.getElementById('gardengateTo').textContent = "Prototypes (Click to switch to wireframes)";
+    document.getElementById('gardengateTo').textContent = "Show Wireframes";
   } else {
      wireframeGG1.style.backgroundImage = "url('./images/gardengateW1.png')";
     wireframeGG2.style.backgroundImage = "url('./images/gardengateW2.png')";
     wireframeGG3.style.backgroundImage = "url('./images/gardengateW3.png')";
     wireframeGG4.style.backgroundImage = "url('./images/gardengateW4.png')";
     wireframeGG5.style.backgroundImage = "url('./images/gardengateW5.png')";
-    document.getElementById('gardengateTo').textContent = "Wireframes (Click to switch to prototypes)";
+    document.getElementById('gardengateTo').textContent = "Show Prototypes";
   }
 }
 
@@ -91,20 +91,20 @@ function switchTo3() {
   let wireframeTZ3 = document.getElementById('travelzW3');
   let wireframeTZ4 = document.getElementById('travelzW4');
   let wireframeTZ5 = document.getElementById('travelzW5');
-  if (document.getElementById('travelzTo').textContent === 'Wireframes (Click to switch to prototypes)') {
+  if (document.getElementById('travelzTo').textContent === 'Show Prototypes') {
     wireframeTZ1.style.backgroundImage = "url('./images/travelzPr1.png')";
     wireframeTZ2.style.backgroundImage = "url('./images/travelzPr2.png')";
     wireframeTZ3.style.backgroundImage = "url('./images/travelzPr3.png')";
     wireframeTZ4.style.backgroundImage = "url('./images/travelzPr4.png')";
     wireframeTZ5.style.backgroundImage = "url('./images/travelzPr5.png')";
-    document.getElementById('travelzTo').textContent = "Prototypes (Click to switch to wireframes)";
+    document.getElementById('travelzTo').textContent = "Show Wireframes";
 } else {
     wireframeTZ1.style.backgroundImage = "url('./images/travelzW1.png')";
     wireframeTZ2.style.backgroundImage = "url('./images/travelzW2.png')";
     wireframeTZ3.style.backgroundImage = "url('./images/travelzW3.png')";
     wireframeTZ4.style.backgroundImage = "url('./images/travelzW4.png')";
     wireframeTZ5.style.backgroundImage = "url('./images/travelzW5.png')";
-    document.getElementById('travelzTo').textContent = "Wireframes (Click to switch to prototypes)";
+    document.getElementById('travelzTo').textContent = "Show Prototypes";
 }
 
 }
